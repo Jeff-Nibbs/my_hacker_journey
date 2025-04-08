5485cbac5fa8ea15dcade9c38ff78ac4739ebeb8
Jeffrey Nibbs <djcountry100@gmail.com>
2025-04-08 15:29:05 -0700

    finally working

--- ipsweeper.sh
+++ ipsweeper.sh
@@ -1,6 +1,12 @@
 #! /bin/zsh
 
 
+
+
+if [[ "$1" == "" ]]; then
+echo "Hey you missed the ip address"
+else
 for ip in `seq 1 254`; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
+fi
