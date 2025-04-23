6f21dd4aceb2ec20cac171be7ae75ab7aae15274
Jeffrey Nibbs <djcountry100@gmail.com>
2025-04-23 14:30:48 -0700

    fix keys blunder

--- check_weather.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-temp=".main.temp"
-description=".weather.[0].description"
-curl -s "https://api.openweathermap.org/data/2.5/weather?lat=38.807930&lon=-121.273643&appid=e4ff88b7b1eaec71ccd662985f2f3a74&units=imperial" | jq $temp,$description
-
-
-
-
