f3636fb1f1b5ee2eb092ac4d677d689e1243abb7
Jeffrey Nibbs <djcountry100@gmail.com>
2025-04-27 12:53:52 -0700

    add wind in forcast

--- weather_app/forcast.sh
+++ weather_app/forcast.sh
@@ -6,17 +6,49 @@ my_lon="-121.273643"
 store_lat="38.788261"
 store_lon="-121.206551"
 
+nesw() {
+  local maths=$(( ( $1 / 90 ) % 4 ))
+  echo "$maths"
+}
+
+find_heading() {
+  if [[ $1 == 0 ]]; then
+    echo "North"
+  elif [[ $1 == 1 ]]; then
+    echo "East"
+  elif [[ $1 == 2 ]]; then
+    echo "South"
+  elif [[ $1 == 3 ]]; then
+    echo "West"
+  else
+    echo "The code is broken"
+  fi
+}
+
 curl -s "https://api.openweathermap.org/data/2.5/forecast?lat=$my_lat&lon=$my_lon&appid=$WEATHER_KEY&units=imperial" > hack_stuff/weather_app/cahe_forcast.txt
 
 for i in {39..0};
 do
   time=$( cat hack_stuff/weather_app/cahe_forcast.txt | jq ".list.[$i | tonumber].dt" )
+ 
   temp=$( cat hack_stuff/weather_app/cahe_forcast.txt | jq ".list.[$i | tonumber].main.temp" )
+  
   desc=$( cat hack_stuff/weather_app/cahe_forcast.txt | jq ".list.[$i | tonumber].weather.[0].description" )
+  
+  windSpeed=$( cat hack_stuff/weather_app/cahe_forcast.txt | jq ".list.[$i | tonumber].wind.speed" )
+  
+  windDirection=$( cat hack_stuff/weather_app/cahe_forcast.txt | jq ".list.[$i | tonumber].wind.deg" )
+  
+  wd=$(nesw $windDirection)
+  direction=$(find_heading $wd)
+
+  temp_imperial="°F"
+  wind_imperial="mph"
+
   echo " "
   echo "$( date -r $time "+| %a %b %d ~ %I:%M %p |" )"
   echo " "
-  echo "      $temp °F"
+  echo "      ${temp}${temp_imperial} Wind Speed: ${windSpeed}${wind_imperial} ${direction}"
   echo "      $desc"
   echo " "
 done
