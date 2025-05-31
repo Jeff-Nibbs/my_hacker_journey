69506570dee3a3435ae17a8814c63deb812a8b6e
Jeffrey Nibbs <djcountry100@gmail.com>
2025-05-31 10:59:00 -0700

    fixed bash scripts

---
+++ over_the_wire/save_keys.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "bandit$1 - $2" >> hack_stuff/over_the_wire/keys.txt
--- start_day.sh
+++ start_day.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 for x in $(cat hack_stuff/text_files/hacker_websites.txt)
 do
