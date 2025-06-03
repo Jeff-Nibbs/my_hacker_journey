29817fce2e4dcd9671e2396bd0fd05c80428aa34
Jeffrey Nibbs <98377720+Jeff-Nibbs@users.noreply.github.com>
2025-06-02 23:12:06 -0700

    fix full file path so aliases work form anywhere

--- start_day.sh
+++ start_day.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for x in $(cat hack_stuff/text_files/hacker_websites.txt)
+for x in $(cat /Users/jnibbs/hack_stuff/text_files/hacker_websites.txt)
 do
     open -a "google chrome" $x
 done
