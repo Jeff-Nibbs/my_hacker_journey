0b0dad753f13f2221afd4fe50e885cf4810feec1
Jeffrey Nibbs <danielnibbs100@gmail.com>
2025-09-08 22:38:43 -0700

    full moon

--- weather_app/weather_now.sh
+++ weather_app/weather_now.sh
@@ -1,13 +1,54 @@
 #!/usr/bin/env bash
 
-folder_path="/Users/jnibbs/hack_stuff/weather_app"
+folder_path="/Users/jnibbs/hack_stuff/weather_app/"
 
 format_date () {
-  echo "$( date -r $1 "+| %a %b %d ~ %I:%M %p |" )"  
+  echo "$( date -r $1 '+| %a %b %d ~ %I:%M %p |' )"  
+}
+
+center() {
+    local term_width
+    term_width=$(tput cols)
+
+    if [ -t 0 ]; then
+        # Input from argument
+        local value="$1"
+        local value_width=${#value}
+        local padding=$(( (term_width - value_width) / 2 ))
+
+        if (( padding > 0 )); then
+            printf "%*s%s\n" "$padding" "" "$value"
+        else
+            echo "$value"
+        fi
+    else
+        # Input from pipe (multi-line)
+        while IFS= read -r value; do
+            local value_width=${#value}
+            local padding=$(( (term_width - value_width) / 2 ))
+
+            if (( padding > 0 )); then
+                printf "%*s%s\n" "$padding" "" "$value"
+            else
+                echo "$value"
+            fi
+        done
+    fi
+}
+
+check () {
+    val="$1"
+    length=${#val}
+    width=$(( COLUMNS - 10 ))
+    if (( length > COLUMNS )); then
+        center "Naw dawg this quote too long"
+    else
+        center "$val"
+    fi
 }
 
 format_time () {
-  echo "$( date -r $1 "+%I:%M %p" )"
+  echo "$( date -r $1 '+%I:%M %p' )"
 }
 
 my_lat="38.807930"
@@ -20,17 +61,23 @@ quote=$( curl -s "https://api.adviceslip.com/advice" | jq ".slip.advice" )
 
 kanye_quote=$( curl -s "https://api.kanye.rest" | jq ".quote" )
 
-curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$my_lat&lon=$my_lon&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_my_weather.txt
+# curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$my_lat&lon=$my_lon&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_my_weather.txt
 
-curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$store_lat&lon=$store_lon&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_store_weather.txt
+curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$store_lat&lon=$store_lon&exclude=current,minutely,hourly,alerts&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_store_weather.txt
+
+curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$my_lat&lon=$my_lon&exclude=current,minutely,hourly,alerts&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_my_weather.txt
 
 my_current_temp=$( cat $folder_path/cahe_my_weather.txt | jq ".main.temp" )
+
+my_current_max_temp=$( cat $folder_path/cahe_my_weather.txt | jq ".main.temp_max" )
 
 my_current_desc=$( cat $folder_path/cahe_my_weather.txt | jq ".weather.[0].description" | tr -d '"' )
 
 my_current_desc=$( cat $folder_path/cahe_my_weather.txt | jq ".weather.[0].description" | tr -d '"' )
 
 store_current_temp=$( cat $folder_path/cahe_store_weather.txt | jq ".main.temp" )
+
+store_current_max_temp=$( cat $folder_path/cahe_store_weather.txt | jq ".main.temp_max" )
 
 store_current_desc=$( cat $folder_path/cahe_store_weather.txt | jq ".weather.[0].description" | tr -d '"' )
 
@@ -48,27 +95,29 @@ fahrenheit="°F"
 am="AM"
 pm="PM"
 
-figlet -w 100 -c -f Merlin1 "Weather"
 echo " "
-echo "                                   $( format_date $time )"
+figlet WEATHER | center
+center "$( format_date $time )"
 echo " "
-echo "                                              Home"
-echo "                                        Temp: ${my_current_temp}${fahrenheit}"
-echo "                                  Feels Like: ${my_feels_like}${fahrenheit}"
-echo "                                       $my_current_desc outside"
+center "Home"
+center "Temp: ${my_current_temp}${fahrenheit}"
+center "Feels Like: ${my_feels_like}${fahrenheit}"
+center "Max Temp: ${my_current_max_temp}${fahrenheit}"
+center  "$my_current_desc outside"
 echo " "
-echo "                                              Work"
-echo "                                        Temp: ${store_current_temp}${fahrenheit}"
-echo "                                  Feels Like: ${store_feels_like}${fahrenheit}"
-echo "                                       $store_current_desc outside"
+center "Work"
+center "Temp: ${store_current_temp}${fahrenheit}"
+center "Feels Like: ${store_feels_like}${fahrenheit}"
+center "Max Temp: ${store_current_max_temp}${fahrenheit}"
+center "$store_current_desc outside"
 echo " "
-echo "                                     Sunrise: $( format_time $sunrise )"
-echo "                                      Sunset: $( format_time $sunset )"
+center "Sunrise: $( format_time $sunrise )"
+center "Sunset: $( format_time $sunset )"
 echo " "
-echo "                            Quote: $quote"
+check "Quote: $quote"
 echo " "
 echo " "
-echo "                      Kanye Quote: $kanye_quote"
+check "Kanye Quote: $kanye_quote"
 echo " "
 
 
